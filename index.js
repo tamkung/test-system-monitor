456f802544c345b06df55d95149025996486deea
Tamkung <hm82529@gmail.com>
2023-08-12 21:05:17 +0700

    [update]fix cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var app = require('express')();
+var cors = require('cors');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var os = require('os');
 var diskspace = require('diskspace');
 var cpu = require('./cpu.js');
+app.use(cors());
 http.listen(3000, function () {
     console.log('listening on *:3000');
 });
